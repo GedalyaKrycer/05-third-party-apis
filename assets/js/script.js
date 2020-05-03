2670d9b9279d9a2914c48af778dde1b2a900d48f
Gedalya Krycer <gedalya@krycer.com>
2020-05-02 19:46:28 -0700

    Added object that assigns hours to each textArea element. Added var that converts the object into an array. Added a loop to cycle through said array and update the css class of the textAreas. Correct classes are not correctly applying however.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,68 @@
+// Saves current time to variable
+var now = moment();
+console.log(now.set('hour', 9))
+
+// Array to capture all of the TextArea Id
+let nineAmInput = $("#nineAmInput"); 
+let tenAmInput = $("#tenAmInput"); 
+let elevenAmInput = $("#elevenAmInput"); 
+let twelvePmInput = $("#twelvePmInput"); 
+let onePmInput = $("#onePmInput"); 
+let twoPmInput = $("#twoPmInput"); 
+let threePmInput = $("#threePmInput"); 
+let fourPmInput = $("#fourPmInput"); 
+let fivePmInput = $("#fivePmInput"); 
+
+
+// Assigns a time to each textArea Element in an object
+let textAreaTimeDesignations = {
+    nineAmInput: now.set('hour', 9),
+    tenAmInput: now.set('hour', 10),
+    elevenAmInput: now.set('hour', 11),
+    twelvePmInput: now.set('hour', 12),
+    onePmInput: now.set('hour', 13),
+    twoPmInput: now.set('hour', 14),
+    threePmInput: now.set('hour', 15),
+    fourPmInput: now.set('hour', 16),
+    fivePmInput: now.set('hour', 20)
+};
+
+// Creates an array from the object above
+let textAreaTimeArray = Object.values(textAreaTimeDesignations);
+
+console.log(textAreaTimeArray);
+
+
+
+// —————————————————————————————————————————————————————————————————————
+
+
 // Displays current date in the header using moment.js API
 $("#currentDay").text(moment().format('dddd ll'));
 
-// Array to capture all of the TextArea Id
-var textAreaArray = [
-    $("#nineAmInput"), 
-    $("#tenAmInput"),
-    $("#elevenAmInput"),
-    $("#twelvePmInput"),
-    $("#onePmInput"),
-    $("#twoPmInput"),
-    $("#threePmInput"),
-    $("#fourPmInput"),
-    $("#fivePmInput")
-]; 
+
+// —————————————————————————————————————————————————————————————————————
+
+
+// Loops through textArea hours 
+for (let i of textAreaTimeArray) {
+    if (now === now) {
+
+        // Sets background to red
+        $(textAreaTimeArray[i]).addClass("present");
+    } else if (now === now.isAfter()) {
+        
+        // Sets background to green
+        $(textAreaTimeArray[i]).addClass("future");
+    } else {
+        
+        // Sets background to grey
+        $(textAreaTimeArray[i]).addClass("past");
+    }
+    
+};
+
+
 
 
 
